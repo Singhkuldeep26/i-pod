71c9a95dd44dcca20a2a352f9625e708e78fc80e
kuldeep singh <pssingh1145@gmail.com>
2023-03-01 22:01:17 +0530

    final

---
+++ src/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./stay.mp3.mp3";
+import music2 from "./deserve-you.mp3.mp3";
+import music3 from "./yummy.mp3.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
